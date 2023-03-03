edcebc64ca7087d735b72d11731c792518499382
Mathieu Segaud <segaud.mathieu@gmail.com>
2023-03-03 19:17:33 +0100

    modif du footer et ajout variables d'environnement

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
 import newsLetter from "../assets/newsletter.png";
+import { db } from "../firebase";
 
 const Footer = () => {
+	const [mailNewsletter, setMailNewsletter] = useState("");
+
 	const year = new Date().getFullYear();
+
+	const addMemberToNewsletter = async (e) => {
+		e.preventDefault();
+		try {
+			const docRef = await addDoc(collection(db, "members"), {
+				mail: mailNewsletter,
+			});
+			console.log("Document créé avec l'id : ", docRef.id);
+		} catch (e) {
+			console.error(
+				"Une erreur est survenue lors de l'ajout du document :",
+				e
+			);
+		}
+
+		setMailNewsletter("");
+		alert("Votre adresse e-mail a bien été ajoutée !");
+	};
 
 	return (
 		<footer>
@@ -17,13 +39,21 @@ const Footer = () => {
 							Pour ne rien rater de nos activités, inscris toi à
 							notre newsletter !{" "}
 						</p>
-						<input
-							type="text"
-							name="mail"
-							id="mail"
-							placeholder="adresse e-mail"
-						/>
-						<button type="submit">Valider</button>
+						<form
+							onSubmit={(e) => addMemberToNewsletter(e)}
+						>
+							<input
+								type="email"
+								id="email"
+								placeholder="adresse e-mail"
+								value={mailNewsletter}
+								onChange={(e) =>
+									setMailNewsletter(e.target.value)
+								}
+								required
+							/>
+							<input type="submit" value="valider" />
+						</form>
 					</div>
 
 					<div className="copyright">
